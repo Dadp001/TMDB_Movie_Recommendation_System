c917aacd3dc9346178efaccc4dc51e2cf749b8f7
Pranjal <pranjal357kumar@gmail.com>
2026-01-09 08:27:48 +0530

    Fix api base url

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import streamlit as st
 # =============================
 # CONFIG
 # =============================
-API_BASE = "https://movie-rec-466x.onrender.com" or "http://127.0.0.1:8000"
+API_BASE = (
+    "https://tmdb-movie-recommendation-system-lgt7.onrender.com"
+    or "http://127.0.0.1:8000"
+)
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
